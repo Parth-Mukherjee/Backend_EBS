5723fc313c3f6b99cd31d7a5745ba79d516de1a7
Parth <parthmukherjee05@gmail.com>
2022-02-03 14:24:09 +0530

    Updated dto

--- Test/ElectricityBillingSystem/DTOs/CreateBillDTO .cs
+++ Test/ElectricityBillingSystem/DTOs/CreateBillDTO .cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace ElectricityBillingSystem.DTO
+﻿namespace ElectricityBillingSystem.DTO
 {
     public class CreateBillDTO
     {
