8996453a7b184fee605780af0606bcf578f3a082
Parth <parthmukherjee05@gmail.com>
2022-02-03 11:58:32 +0530

    Backend Updated

---
+++ Test/ElectricityBillingSystem/Controllers/AccountLogInController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using ElectricityBillingSystem.Repositories;
+using ElectricityBillingSystem.Models;
+using ElectricityBillingSystem.Entities;
+using ElectricityBillingSystem.DTO;
+
+namespace ElectricityBillingSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] 
+    public class AccountLogInController : ControllerBase
+    {
+        private IAdminRepository _adminRepository = null;   
+        private ICustomerRepository _customerRepository = null;
+        //Constructor
+        public AccountLogInController(ICustomerRepository customerRepository,IAdminRepository adminRepository)
+        {
+            _adminRepository = adminRepository;
+            _customerRepository = customerRepository;
+        }
+
+        //Logging in Admin
+        [Route("Login")]
+        [HttpPost]
+        public IActionResult Login([FromBody]LoginModel login)
+        {
+            if (login == null)
+            {
+                return BadRequest("Invalid client request");
+            }
+            LoggedUserModel model = new LoggedUserModel();
+            //Validating Login credentials
+            Admin admin = _adminRepository.ValidateAdmin(login);
+            if (admin != null)
+            {
+                string token = _adminRepository.GetTokenForAdmin(admin);
+                model = new LoggedUserModel() { Id = admin.AdminId, EmailID = admin.AdminEmail, Token = token, Role = admin.Role };
+                return Ok(model);
+            }
+            else
+            {
+                Customer customer = _customerRepository.ValidateCustomer(login);
+                if (customer != null)
+                {
+                    string token = _customerRepository.GetTokenForCustomer(customer);
+                    model = new LoggedUserModel() { Id = customer.CustomerId, EmailID = customer.CustomerEmail, Token = token, Role = customer.Role };
+                    return Ok(model);
+                }
+                else
+                {
+                    return BadRequest("Invalid Credentials");
+                }
+            }
+        }
+
+        [Route("ForgetPassword")]
+        [HttpPut]
+        public IActionResult ForgetPassword([FromBody] ForgetPasswordDTO forgetPassword)
+        {
+            if (forgetPassword.NewPassword == forgetPassword.ConfirmPassword) {
+                if (forgetPassword.Role == "ADMIN")
+                {
+                    Feedback feedback = _adminRepository.ForgetPassword(forgetPassword);
+                    if (feedback.Result == true)
+                    {
+                        return Ok(feedback);
+                    }
+                    else
+                    {
+                        return BadRequest(feedback);
+                    }
+                }
+                else if (forgetPassword.Role == "CUSTOMER")
+                {
+                    Feedback feedback = _customerRepository.ForgetPassword(forgetPassword);
+                    if (feedback.Result == true)
+                    {
+                        return Ok(feedback);
+                    }
+                    else
+                    {
+                        return BadRequest(feedback);
+                    }
+                }
+                else
+                {
+                    return BadRequest("Invalid type of User!");
+                }
+            }
+            else {
+                return BadRequest("Password doesnot match with Confirm Password!");
+            }
+           
+            //Validating Login credentials
+
+        }
+
+        [HttpPost]
+        [Route("CustomerRegistration")]
+        public IActionResult CustomerRegistration([FromBody] CustomerRegDTO customerRegDTO)
+        {
+            try
+            {
+                Feedback feedback = _customerRepository.Register(customerRegDTO);
+                return Ok(feedback);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ Test/ElectricityBillingSystem/Models/Feedback.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace ElectricityBillingSystem.Models
+{
+    public class Feedback
+    {
+        //Feedback result having a boolean value
+        public bool Result { get; set; }
+
+        //Feedback message
+        public string Message { get; set; }
+    }
+}
